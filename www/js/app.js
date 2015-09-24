0c77c50bad9a81999dea52692ad4f32cabb5af45
Harun Oral <harun.oral@web.de>
2015-09-24 06:05:21 +0200

    Angular UI + Bootstrap (before Injection)

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var votex = angular.module('starter', ['ionic','firebase','starter.controllers', 'starter.profileCtrl', 'ngCordova','ngMessages','ionic.ion.imageCacheFactory'])
+var votex = angular.module('starter', ['ionic','firebase','starter.controllers', 'starter.profileCtrl','ui.bootstrap', 'ngCordova','ngMessages','ionic.ion.imageCacheFactory'])
 
 votex.run(function($ionicPlatform, $cordovaSplashscreen, $cordovaStatusbar) {
   $ionicPlatform.ready(function() {
