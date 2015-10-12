e25488d567576c10f1aa42bd2e8ab3ffc6fdccc9
Harun Oral <harun.oral@web.de>
2015-10-12 23:12:50 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 3a412803db22377bf4cfe42f3ea4e05369b11c1a, reversing
    changes made to df3b0cdfa999eaf480da82c2f35c7452293b5dc7.

--- www/js/profileCtrl.js
+++ www/js/profileCtrl.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angular.module('starter.profileCtrl', ['firebase'])
 
 .controller('profileCtrl', function ($scope, $firebaseAuth) {
@@ -30,34 +29,3 @@ angular.module('starter.profileCtrl', ['firebase'])
           
 	 
   });
-=======
-angular.module('profileCtrl', ['firebase'])
-
-.controller('profileCtrl', function($scope, $firebaseAuth) {
-
-
-
-  $scope.VotesCount = 9001;
-  $scope.username = 'Harun Oral';
-  $scope.lastDate = '06.10.2015';
-  $scope.level = 'Admin & Experte';
-
-  //---------------SMS VERIF---------------------
-  var sendSMSText = function(recipient) {
-    var newFb = new Firebase("https://vote-x.firebaseio.com/users/" + $scope.userID);
-    var smsQueue = newFb.child("/sms/" + recipient.phone);
-
-    var personalizedText = "Hallo " + recipient.name + ". Danke für's verifizieren !" + "./n/n"
-    "Wir wünschen Dir noch viel Spaß, dein Vote-X Team :)";
-
-    smsQueue.set({
-      name: recipient.name,
-      phone: recipient.phone,
-      text: personalizedText
-
-    });
-  }
-
-
-});
->>>>>>> origin/master
