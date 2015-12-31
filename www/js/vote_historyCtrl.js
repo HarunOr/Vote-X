bb70682dce7aab35f7119cf6f81ebcdb2560f9b2
Harun Oral <harun.oral@web.de>
2015-12-31 10:58:18 +0100

    vote_history works :)

--- www/js/vote_historyCtrl.js
+++ www/js/vote_historyCtrl.js
@@ -1,8 +1,15 @@
  angular.module('starter.vote_historyCtrl', ['firebase','google.places'])
-.controller("vote_historyCtrl", function ($scope,$rootScope,$ionicLoading, $firebaseArray) {
-	var userVoteRef = new Firebase("https://vote-x.firebaseio.com/users/"+$rootScope.userInfo.uid+"/vote_history");
+.controller("vote_historyCtrl", function ($scope,$rootScope,$ionicLoading, $firebaseArray, $timeout) {
+	
     
-    $scope.votedPlace= [];
+    if($rootScope.currentUserSignedIn){
+        
+   
+    var userVoteRef = new Firebase("https://vote-x.firebaseio.com/users/"+$rootScope.userInfo.uid+"/vote_history");
+    $rootScope.counter = 0;
+    $scope.votedPlace = [25];
+    $scope.votedPlace[$rootScope.counter]= {id: 0, placeObject: "", voteText: "", votePoints: "", voteTime: "", voteUpvotes: 0};
+
     
     
     userVoteRef.on("child_added", function(snapshot){
@@ -13,27 +20,31 @@
    
 var votedPlaceRef = new Firebase("https://vote-x.firebaseio.com/places/"+snapshot.key()+"/votes/"+snapshot.val());   
 votedPlaceRef.once("value", function(childSnapshot){
+   
     var voteData = childSnapshot.val();
-    $scope.votePoints = voteData.vote_points;
-    $scope.voteUpvotes = voteData.vote_upvotes;
-    $scope.voteText = voteData.description;
-    $scope.voteTime = voteData.vote_time;
-    console.info("$scope.votePoints "+$scope.votePoints);
-    console.info("$scope.voteText "+$scope.voteText);
-    
-})   
-   var placeId = snapshot.key();
+       var placeId = snapshot.key();
+       
    var geocoder = new google.maps.Geocoder;
    geocoder.geocode({'placeId': placeId}, function(results, status) {
       if (status === google.maps.GeocoderStatus.OK) {
       if (results[0]) {
-          $scope.votedPlace.push(results[0]);
-        console.info($scope.votedPlace);
+          $scope.$apply(function(){
+             $scope.votedPlace[$rootScope.counter] = {id: $rootScope.counter, placeObject: results[0], voteText:voteData.description,votePoints: voteData.vote_points, voteTime:voteData.vote_time,voteUpvotes: voteData.vote_upvotes};  
+              
+          })
+        
+
+          $rootScope.counter++;
+
       }}
    })
+})   
+    $timeout(function(){
+     $ionicLoading.hide();      
+    },500);
    
-     $ionicLoading.hide();   
+     
     });
     
-    
+     }
 })
